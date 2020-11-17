8cabe4ff9e0d2aa929cd32708e12aa80d41d4113
codexempire <princewillifeanyi1999@gmail.com>
2020-11-17 13:11:44 +0100

    Working on the landing page of ChatUP

---
+++ ChatUP-web/src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  styleUrls: ['./landing-page.component.css'],
+  templateUrl: './landing-page.component.html',
+})
+export class LandingPageComponent implements OnInit {
+  image;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
